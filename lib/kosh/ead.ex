ea850fe85bd7bdda63f938636d2bb41d4b0297f6
dennyabrain <denny.george90@gmail.com>
2025-05-09 11:37:35 +0530

    refactor: xml parsers

---
+++ lib/kosh/ead.ex
@@ -0,0 +1,12 @@
+defmodule Kosh.EAD do
+  @moduledoc """
+  Context for handling EAD documents
+  """
+
+  def load(filepath) do
+    # map = XML.parse(filepath)
+    # struct = DataModel.new(map)
+    # struct.EADHeader.EADID.countrycode
+    # struct.Archdesc.did.repository.corpname
+  end
+end
