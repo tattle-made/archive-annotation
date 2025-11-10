8d054ff0e3ed9c5efcaaa9f7df693fb2be028fa9
maanasb01 <maanasb01@gmail.com>
2025-11-10 14:40:18 +0530

    feat: add oban and configure it

--- config/test.exs
+++ config/test.exs
@@ -38,3 +38,6 @@ config :phoenix, :plug_init_mode, :runtime
 # Enable helpful, but potentially expensive runtime checks
 config :phoenix_live_view,
   enable_expensive_runtime_checks: true
+
+# To prevent Oban from running jobs and plugins during test runs, enable :testing mode in test.exs
+config :kosh, Oban, testing: :manual
