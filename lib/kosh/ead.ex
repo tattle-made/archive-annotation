41f3d1316ab330f6d3fb10d1de2489ddc6eec6f1
maanasb01 <maanasb01@gmail.com>
2025-12-17 00:43:32 +0530

    feat: implement syncing on the App

--- lib/kosh/ead.ex
+++ lib/kosh/ead.ex
@@ -1,5 +1,16 @@
 defmodule Kosh.EAD do
-  alias Kosh.EAD.{Subject, XML.Saxmap, Collection, Series, SubSeries, File, Agent, LcnafType, DefinedEmotion}
+  alias Kosh.EAD.{
+    Subject,
+    XML.Saxmap,
+    Collection,
+    Series,
+    SubSeries,
+    File,
+    Agent,
+    LcnafType,
+    DefinedEmotion
+  }
+
   alias Kosh.Repo
   import Ecto.Query
   alias Kosh.EAD.XML.SaxyUpdateEadHandler
@@ -61,6 +72,13 @@ defmodule Kosh.EAD do
   def get_collection(collection_id) do
     Collection
     |> Repo.get(collection_id)
+  end
+
+  def get_collection_from_uri(uri) do
+    from(c in Collection,
+      where: c.unitid["uri"] == ^uri
+    )
+    |> Repo.one()
   end
 
   @doc """
@@ -137,6 +155,106 @@ defmodule Kosh.EAD do
     %Series{}
     |> Series.changeset(attrs)
     |> Repo.insert()
+  end
+
+  def get_series_from_uri(uri) do
+    from(s in Series,
+      where: s.unitid["uri"] == ^uri
+    )
+    |> Repo.one()
+  end
+
+  def get_sub_series_from_uri(uri) do
+    from(s in SubSeries,
+      where: s.unitid["uri"] == ^uri
+    )
+    |> Repo.one()
+  end
+
+  @doc """
+  Searches an existing Sub-Series record from the URI. If not found, then create a new series
+  """
+  def update_sub_series(attrs) do
+    uri = get_in(attrs, [:unitid, :uri])
+
+    if not is_nil(uri) do
+      result =
+        case get_sub_series_from_uri(uri) do
+          nil ->
+            case create_sub_series(attrs) do
+              {:ok, inserted_series} ->
+                {:ok, inserted_series}
+
+              {:error, changeset} ->
+                {:error, changeset}
+            end
+
+          series ->
+            changeset = SubSeries.changeset(series, attrs)
+
+            case Repo.update(changeset) do
+              {:ok, updated} ->
+                {:ok, updated}
+
+              {:error, changeset} ->
+                {:error, changeset}
+            end
+        end
+
+      case result do
+        {:ok, series} ->
+          {:ok, series}
+
+        {:error, changeset} ->
+          {:error, "Error while updating series: #{inspect(changeset)}"}
+      end
+    else
+      {:error,
+       "Error while updating the Sub-Series, URI not found. Sub-Series Attributes: #{inspect(attrs)}"}
+    end
+  end
+
+  @doc """
+  Searches an existing Series record from the URI. If not found, then create a new series
+  """
+  def update_series(attrs) do
+    uri = get_in(attrs, [:unitid, :uri])
+
+    if not is_nil(uri) do
+      result =
+        case get_series_from_uri(uri) do
+          nil ->
+            case create_series(attrs) do
+              {:ok, inserted_series} ->
+                {:ok, inserted_series}
+
+              {:error, changeset} ->
+                {:error, changeset}
+            end
+
+          series ->
+            changeset = Series.changeset(series, attrs)
+
+            case Repo.update(changeset) do
+              {:ok, updated} ->
+                {:ok, updated}
+
+              {:error, changeset} ->
+                {:error, changeset}
+            end
+        end
+
+      case result do
+        {:ok, series} ->
+          {:ok, series}
+
+        {:error, changeset} ->
+          {:error, "Error while updating series: #{inspect(changeset)}"}
+      end
+    else
+      {:error,
+       "Error while updating the Series, URI not found. Series Attributes: #{inspect(attrs)}"}
+    end
   end
 
   # SubSeries functions
@@ -440,6 +558,227 @@ defmodule Kosh.EAD do
     end
   end
 
+  def update_data(processed_map, tmp_path, dest_full) do
+    upload_path = "/uploads/" <> Path.basename(dest_full)
+
+    with {:ok, {collection_map, nested_structure}} <-
+           Saxmap.extract_contents_from_processed_map(processed_map) do
+      update_ead_contents({collection_map, nested_structure}, tmp_path, dest_full, upload_path)
+    else
+      {:error, reason} -> {:error, reason}
+      error -> {:error, "Unexpected error: #{inspect(error)}"}
+    end
+  end
+
+  def update_collection(%Collection{} = collection, subjects, changeset_attrs) do
+    case Repo.transaction(fn ->
+           collection = Repo.preload(collection, :subjects)
+           changeset = Collection.changeset(collection, changeset_attrs)
+
+           case Repo.update(changeset) do
+             {:ok, updated} ->
+               if subjects in [nil] do
+                 updated
+               else
+                 case add_subjects_to_collection(updated, subjects) do
+                   {:ok, _} ->
+                     Repo.get!(Collection, updated.id) |> Repo.preload(:subjects)
+
+                   {:error, reason} ->
+                     Repo.rollback(reason)
+                 end
+               end
+
+             {:error, changeset} ->
+               Repo.rollback(changeset)
+           end
+         end) do
+      {:ok, collection} -> {:ok, collection}
+      {:error, reason} -> {:error, reason}
+    end
+  end
+
+  @spec update_file(any(), any()) :: {:error, any()} | {:ok, any()}
+  def update_file(subjects, changeset_attrs) do
+    case get_file_from_uri(changeset_attrs.uri) do
+      nil ->
+        case create_file(changeset_attrs) do
+          {:ok, inserted_file} ->
+            case add_subjects_to_file(inserted_file, subjects) do
+              {:ok, _} -> {:ok, inserted_file}
+              {:error, reason} -> {:error, "Failed to add subjects to file: #{reason}"}
+            end
+
+          {:error, changeset} ->
+            {:error, "Failed to create file: #{inspect(changeset.errors)}"}
+        end
+
+      file ->
+        case Repo.transaction(fn ->
+               file = Repo.preload(file, :subjects)
+               changeset = File.changeset(file, changeset_attrs)
+
+               case Repo.update(changeset) do
+                 {:ok, updated} ->
+                   if subjects in [nil] do
+                     updated
+                   else
+                     case add_subjects_to_file(updated, subjects) do
+                       {:ok, _} ->
+                         Repo.get!(File, updated.id) |> Repo.preload(:subjects)
+
+                       {:error, reason} ->
+                         Repo.rollback(reason)
+                     end
+                   end
+
+                 {:error, changeset} ->
+                   Repo.rollback(changeset)
+               end
+             end) do
+          {:ok, file} -> {:ok, file}
+          {:error, reason} -> {:error, reason}
+        end
+    end
+  end
+
+  defp update_nested_structure(nodes, collection_id, series_id, sub_series_id)
+       when is_list(nodes) do
+    try do
+      Enum.each(nodes, fn node ->
+        case update_node_for_db(node, collection_id, series_id, sub_series_id) do
+          {:ok, _} -> :ok
+          {:error, reason} -> throw({:error, reason})
+        end
+      end)
+
+      :ok
+    catch
+      {:error, reason} -> {:error, reason}
+    end
+  end
+
+  defp update_node_for_db(%{type: :series} = node, collection_id, _series_id, _sub_series_id) do
+    series_attrs =
+      node
+      |> Map.drop([:type, :children])
+      |> Map.put(:collection_id, collection_id)
+
+    # Insert series
+    case update_series(series_attrs) do
+      {:ok, updated_series} ->
+        case update_nested_structure(node.children, collection_id, updated_series.id, nil) do
+          :ok -> {:ok, updated_series}
+          {:error, reason} -> {:error, reason}
+        end
+
+      {:error, reason} ->
+        {:error, "Failed to create series: #{inspect(reason)}"}
+    end
+  end
+
+  defp update_node_for_db(%{type: :subseries} = node, collection_id, series_id, _sub_series_id) do
+    # Drop non-schema fields and add collection_id and series_id
+    sub_series_attrs =
+      node
+      |> Map.drop([:type, :children])
+      |> Map.merge(%{
+        collection_id: collection_id,
+        series_id: series_id
+      })
+
+    # Insert subseries
+    case update_sub_series(sub_series_attrs) do
+      {:ok, inserted_sub_series} ->
+        case update_nested_structure(
+               node.children,
+               collection_id,
+               series_id,
+               inserted_sub_series.id
+             ) do
+          :ok -> {:ok, inserted_sub_series}
+          {:error, reason} -> {:error, reason}
+        end
+
+      {:error, reason} ->
+        {:error, "Failed to create sub-series: #{inspect(reason)}"}
+    end
+  end
+
+  defp update_node_for_db(%{type: :file} = node, collection_id, series_id, sub_series_id) do
+    # Drop non-schema fields and add IDs
+    file_uri = node.unitid.uri
+    file_subjects = get_in(node, [:subjects]) || []
+
+    file_attrs =
+      node
+      |> Map.drop([:type, :subjects])
+      |> Map.merge(%{
+        uri: file_uri,
+        collection_id: collection_id,
+        series_id: series_id,
+        sub_series_id: sub_series_id
+      })
+
+    # Insert file
+    case update_file(file_subjects, file_attrs) do
+      {:ok, updated_file} ->
+        {:ok, updated_file}
+
+      {:error, reason} ->
+        {:error, "Failed to update file: #{inspect(reason)}"}
+    end
+  end
+
+  defp update_node_for_db(node, _collection_id, _series_id, _sub_series_id) do
+    {:error, "Unknown node type: #{inspect(node)}"}
+  end
+
+  defp update_ead_contents({collection_map, nested_structure}, tmp_path, dest_full, upload_path) do
+    case Repo.transaction(fn ->
+           subjects = collection_map.subjects
+           attrs = collection_map |> Map.drop([:subjects]) |> Map.put(:upload_path, upload_path)
+
+           with collection_uri when not is_nil(collection_uri) <- get_in(attrs, [:unitid, :uri]),
+                existing_collection when not is_nil(existing_collection) <-
+                  get_collection_from_uri(collection_uri),
+                {:ok, updated_collection} <-
+                  update_collection(existing_collection, subjects, attrs) do
+             case update_nested_structure(nested_structure, updated_collection.id, nil, nil) do
+               :ok -> :ok
+               {:error, reason} -> Repo.rollback("Failed to process nested structure: #{reason}")
+             end
+
+             case Elixir.File.cp(tmp_path, dest_full) do
+               :ok ->
+                 updated_collection
+
+               {:error, reason} ->
+                 Elixir.File.rm(dest_full)
+                 Repo.rollback("Failed to copy file: #{inspect(reason)}")
+             end
+           else
+             nil ->
+               Repo.rollback(
+                 "Failed to update collection, either collection does not exist or Collection URI is missing"
+               )
+
+             {:error, reason} ->
+               Repo.rollback("Failed to update collection, #{inspect(reason)}")
+
+             reason ->
+               Repo.rollback("Failed to update collection, #{inspect(reason)}")
+           end
+         end) do
+      {:ok, updated_collection} ->
+        {:ok, updated_collection}
+
+      {:error, reason} ->
+        if Elixir.File.exists?(dest_full), do: Elixir.File.rm(dest_full)
+        {:error, reason}
+    end
+  end
+
   @spec insert_ead_contents({map(), list()}, String.t(), String.t(), String.t()) ::
           {:ok, struct()} | {:error, String.t()}
   defp insert_ead_contents({collection_map, nested_structure}, tmp_path, dest_full, upload_path) do
@@ -552,7 +891,6 @@ defmodule Kosh.EAD do
   end
 
   defp process_node_for_db(%{type: :file} = node, collection_id, series_id, sub_series_id) do
-    IO.inspect(node, label: "FILE NODE: ")
     # Drop non-schema fields and add IDs
     file_uri = node.unitid.uri
     file_subjects = get_in(node, [:subjects]) || []
