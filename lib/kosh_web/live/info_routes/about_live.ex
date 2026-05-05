47f9dee47c8dbd07a51d8292cf7af5264ae0b973
ancbs-dev <archives-dev@ncbs.res.in>
2026-05-05 15:27:16 +0530

    updates

--- lib/kosh_web/live/info_routes/about_live.ex
+++ lib/kosh_web/live/info_routes/about_live.ex
@@ -13,12 +13,16 @@ alias Kosh.EAD.Model.EAD.ArchDesc.DID.Repository
         About
       </div>
 
+      <p class="text-primary-purple mt-2 font-bold">Supported by <a class="underline" href="https://arcadiafund.org.uk" target="_blank">Arcadia</a>⧉</p>
+      <p class="text-secondary-purple mt-2">The software architecture for the anno.milli prototype was made possible through an Arcadia-funded project at the Archives at NCBS, to build a public open tool for wider discovery of archival material, drawing on the openly available resources of the Milli network.</p>
+
       <p class="text-primary-purple mt-2 font-bold">Source Repository</p>
       <p class="text-secondary-purple mt-2"><a class="underline" href="https://github.com/tattle-made/archive-annotation" target="_blank">tattle-made/archive-annotation</a>⧉</p>
 
       <p class="text-primary-purple mt-2 font-bold">Development</p>
-      <p class="text-secondary-purple mt-2">Tattle Civic Tech: Maanas B, Aatman V, Denny G, Tarunima P</p>
-      <p class="text-secondary-purple mt-2">Archives at NCBS: Ojas K, Venkat S</p>
+      <p class="text-secondary-purple mt-2">Tattle: Maanas B, Aatman V, Denny G, Tarunima P</p>
+      <p class="text-secondary-purple mt-2">Milli Archives Foundation: Venkat S</p>
+      <p class="text-secondary-purple mt-2">Archives at NCBS: Ojas K</p>
 
       <p class="text-primary-purple mt-2 font-bold">Design</p>
       <p class="text-secondary-purple mt-2">Anoopa John</p>
@@ -27,7 +31,8 @@ alias Kosh.EAD.Model.EAD.ArchDesc.DID.Repository
       <p class="text-secondary-purple mt-2">Caltech Archives and Special Collections: Mariella Soprano, Thomas Keswick</p>
       <p class="text-secondary-purple mt-2">University of Leeds: Liz Stainforth, Dibyadyuti Roy</p>
 
-      <p class="text-primary-purple mt-2 font-bold">Supported by <a class="underline" href="https://arcadiafund.org.uk" target="_blank">Arcadia</a>⧉</p>
+      <p class="text-primary-purple mt-2 font-bold">Past iterations of anno.milli</p>
+      <p class="text-secondary-purple mt-2">Developed by Janastu: Shafali J, Micah A, Bhanu P, and T B Dinesh, with grant support from Foundation for Innovation and Social Entrepreneurship (FISE)</p>
 
     </div>
     """
